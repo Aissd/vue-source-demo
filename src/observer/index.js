e4934127ba57943c93c20de07e71a9dede00316f
zouyushen_cx <zouyushen@richinfo.cn>
2020-06-13 11:20:18 +0800

    使用defineProperty对对象的属性进行劫持，观测

--- src/observer/index.js
+++ src/observer/index.js
@@ -2,14 +2,41 @@ import { isObject } from '../utils.js';
 
 class Observer {
     constructor(data) {
-        console.log(data);
+        this.walk(data); // 可以对数据一步一步的处理
     }
+    walk(data) {
+        // 循环对象，给对象的每个属性添加观测
+        // 对象的循环 data: { msg: 'zf', age: 11 }
+        // 这里不建议用forin循环对象，因为会枚举出原型的东西
+        Object.keys(data).forEach(key => {
+            defineReactive(data, key, data[key]); // 定义响应式的数据变化
+        });
+    }
+}
+
+// 给对象的每个属性添加响应式处理
+// data - 被循环的对象
+// 循环中当前的key
+// 循环中当前的value
+// vue2的性能问题，需要递归重写get和set，性能慢，vue3使用proxy就没这个问题
+function defineReactive(data, key, value) {
+    observe(value); // 若值还是为对象，则用递归，继续观测
+    Object.defineProperty(data, key, {
+        get() {
+            return value;
+        }, 
+        set(newValue) {
+            if(newValue == value) return;
+            if(isObject(newValue)) observe(newValue); // 设置的值有可能也是对象，所以需要判断监控
+            value = newValue;
+        }
+    })
 }
 
 export function observe(data) {
     // 对象就是使用defineProperty来实现响应式原理
     
-    if(!isObject) {
+    if(!isObject(data)) {
         // 如果这个数据不是对象，或者是null，那就不监控
         return;
     }
--- src/state.js
+++ src/state.js
@@ -17,7 +17,8 @@ function initProps(vm) {}
 function initMethods(vm) {}
 function initData(vm) {
     let data = vm.$options.data; // 用户传入的数据
-    data = typeof data === 'function' ? data.call(vm) : data; // 若是函数，则执行（data是函数封装，return一个对象，即获取到了数据）
+    // vm._data 就是观测后的数据（都有get和set标识），暴露到实例上
+    data = vm._data = typeof data === 'function' ? data.call(vm) : data; // 若是函数，则执行（data是函数封装，return一个对象，即获取到了数据）
     // 观测数据，给get，set方法
     observe(data);
 }
