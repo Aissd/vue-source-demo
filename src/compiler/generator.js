b5a48517d2c7aee3a67ae654e3b3324a5100f5c7
zouyushen_cx <zouyushen@richinfo.cn>
2020-06-14 23:36:56 +0800

    编译部分

---
+++ src/compiler/generator.js
@@ -0,0 +1,74 @@
+// 匹配动态变量的  +? 尽可能少匹配
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+
+function genProps(attrs) { // { id: 'app', style: { color: red } }
+    let str = '';
+    for (let i = 0; i < attrs.length; i++) {
+        let attr = attrs[i]; // 取到每一个属性
+        if(attr.name == 'style') {
+            let obj = {};
+            attr.value.split(';').forEach(item => { // 有可能有多个属性：color:red;background:green
+                let [key, value] = item.split(':');
+                obj[key] = value;
+            });
+
+            attr.value = obj; // 将原来的字符串换成了格式化后的对象
+        }
+        str += `${attr.name}:${JSON.stringify(attr.value)},`;
+    }
+    return `{${str.slice(0, -1)}}`; // 这里的splice是去掉最后的逗号
+}
+
+function gen(node) { // <div><span></span>hello</div>
+    if(node.type === 1) {
+        // 1的话，说明是元素
+        return generate(node);    
+    } else {
+        // 文本的处理
+        let text = node.text;
+        if(!defaultTagRE.test(text)) {
+            // 文本里没有变量
+            return `_v(${JSON.stringify(text)})`;
+        } else {
+            // 文本里有变量，例如
+            // helloworld {{msg}} aa {{bb}} => _v('helloworld' + _s(msg) + 'aa' + _s(bb))
+            let tokens = []; // 每次正则使用过后，都需要重新制定lastIndex
+            let lastIndex = defaultTagRE.lastIndex = 0;
+            let match, index; // 匹配后的结果
+            while(match = defaultTagRE.exec(text)) {
+                index = match.index;
+                // 通过lastIndex,index
+                tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+
+                tokens.push(`_s(${match[1].trim()})`);
+
+                lastIndex = index + match[0].length;
+            }
+
+            if(lastIndex < text.length) {
+                tokens.push(JSON.stringify(text.slice(lastIndex)));
+            }
+            return `_v(${tokens.join('+')})`;
+        }
+    }
+}
+
+function genChildren(el) {
+    const children = el.children;
+    // 判断是否有后台
+    if(children) {
+        return children.map(c => gen(c)).join(',') // 生成后代拼接的字符串
+    } else {
+        return false;
+    }
+}
+
+export function generate(el) {
+    let code = `
+        _c("${el.tag}", ${ // 标签
+            el.attrs.length ? `${genProps(el.attrs)}` : undefined // 属性
+        }${
+            el.children ? `,${genChildren(el)}` : '' // 子
+        })`;
+    return code;
+}
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,15 +1,28 @@
 import { parseHTML } from './parser.js';
+import { generate } from './generator.js';
 export function compileToFunctions(template) {
     // 实现模板的编译
 
     let ast = parseHTML(template);
+    
+    // 代码生成 - template => render函数
 
-    console.log(ast);
+    /**
+     * 把这种ast 转换成下面的形式 ： <div id="app" style="color:red"><span>helloworld {{msg}}</span></div>
+     * render() {
+     *  with(this._data) {
+     *      return _c('div', { id: 'app', style: {color: red}}, _c('span', undefined, _v('helloworld' + _s(msg) ) ) );
+     *  }
+     * }
+     */
 
+    // 核心就是字符串拼接
+    let code = generate(ast);
+    console.log(code);
     // 模板编译原理
     // 1、先把我们的代码转化成ast语法树
     //  1.1）parser解析 - 通过正则
-    // 2、标记静态树 - 不会变的<span>123</span> - 对比时，是静态树就会忽略，有利于提高性能
+    // 2、标记静态树 - 不会变的<span>123</span> - 对比时，是静态树就会忽略，有利于提高性能（优化）
     //  2.1）树得遍历标记 markup
     // 3、通过ast产生的语法树，生成代码 => render函数
     //  3.1）codegen
